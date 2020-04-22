ab61b424624511a47d11918a396e62b42be23846
DavidColatti <davidcolatti@gmail.com>
2020-04-22 14:36:53 -0400

    iteration 3 completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import 'bulma/css/bulma.css';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		displayedFoods: foods,
+		foodName: '',
+		foodCalories: '',
+		foodImage: '',
+		showForm: false
+	};
+
+	displayFood() {
+		let foodsArray = [ ...this.state.displayedFoods ];
+
+		return foodsArray.map((food) => {
+			return <FoodBox name={food.name} calories={food.calories} image={food.image} />;
+		});
+	}
+
+	changeFoodName = (e) => {
+		this.setState({
+			foodName: e.target.value
+		});
+	};
+
+	changeFoodCalories = (e) => {
+		this.setState({
+			foodCalories: e.target.value
+		});
+	};
+
+	changeFoodImage = (e) => {
+		this.setState({
+			foodImage: e.target.value
+		});
+	};
+
+	submitFunction = (e) => {
+		e.preventDefault();
+		let foodArray = [ ...this.state.displayedFoods ];
+
+		let newFood = {
+			name: this.state.foodName,
+			calories: this.state.foodCalories,
+			image: this.state.foodImage
+		};
+		foodArray.push(newFood)
+		
+		this.setState({
+			displayedFoods: foodArray,
+			showForm: false
+		})
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<button
+					className="addNewFoodBtn"
+					onClick={() => {
+						this.setState({ showForm: !this.state.showForm });
+					}}
+				>
+					Add New Food
+				</button>
+				{this.state.showForm ? (
+					<form>
+						<input onChange={this.changeFoodName} name="foodName" type="name" placeholder="Food Name" />
+						<input
+							onChange={this.changeFoodCalories}
+							name="foodCalories"
+							type="number"
+							placeholder="Calories"
+						/>
+						<input onChange={this.changeFoodImage} name="foodImage" type="text" placeholder="ImageUrl" />
+						<input type="submit" onClick={this.submitFunction} />
+					</form>
+				) : (
+					''
+				)}
+				{this.displayFood()}
+			</div>
+		);
+	}
 }
 
 export default App;
